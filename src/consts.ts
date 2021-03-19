dea1661e45f0f9d3e848d6f4c5bf0d8cc3a8e103
kunter555 <lagent073@gmail.com>
2021-03-19 12:01:09 +0200

    'test'

---
+++ src/consts.ts
@@ -0,0 +1,22 @@
+const fortniteClassId = 7314;
+
+const interestingFeatures = [
+  'roster',
+  // 'match_info'
+];
+
+const windowNames = {
+  inGame: 'in_game',
+  desktop: 'desktop'
+};
+
+const hotkeys = {
+  toggle: 'showhide'
+};
+
+export {
+  fortniteClassId,
+  interestingFeatures,
+  windowNames,
+  hotkeys
+}
